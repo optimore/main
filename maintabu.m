853046d12b5a89a2cee9072f12fac9163959ae65
Emelie Karlsson <emeka813@maiex-03.edu.mai.liu.se>
2015-10-29 16:22:20 +0100

    Safety commit

--- maintabu.m
+++ maintabu.m
@@ -3,7 +3,8 @@ clc;
 % This funcion is in guitest
 % logPath = CreateLog()
 
-dataParameters.path = 'src/test/testdata/firsttestdataexampel.dat';
+% Old: dataParameters.path = 'src/test/testdata/firsttestdataexampel.dat';
+dataParameters.path = 'src/test/testdata/TimelineAttributes1.dat';
 modelParameters.tabu = {[1,2,3]};
 logParameters.path = 'target/logs/2015-10-28T14-15-31.log';
 logParameters.id = 1;
--- src/main/tabu/GetData.m
+++ src/main/tabu/GetData.m
@@ -1,14 +1,14 @@
-function data=GetData(dataDirectory)
+function data=GetData(dataParameters)
 %% Combind models from parameters
 % This function creates a model from given parameters
 % Created by: Victor Bergelin
 % Date created: 28/10/2015
 % Version number 
-% 0.02: minor development, not tested
+% 0.02: 
 % 0.01: file setup
 % Linköping University, Linköping
 
-data.data = dataDirectory;
+data.data = dataParameters;
 
 data.parameters = 'parameters';
 
