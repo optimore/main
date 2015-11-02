8c03aab9e339b3d164a46a8976f72d33f3852e45
Emelie Karlsson <emeliemariatherese.k@gmail.com>
2015-11-02 17:02:19 +0100

    Working functions in CostFunctions

--- src/main/tabu/CostFunctions/OverlapCost.m
+++ src/main/tabu/CostFunctions/OverlapCost.m
@@ -1,48 +1,56 @@
-function C=OverlapCost(data, model)
-%% Task Interval
-% needs comments!
+function C=OverlapCost(data)
+%% Calulates cost for tasks that are overlapping on timeline
+% Compares the starting and ending time of all tasks in the data.
+% Accumulates result in C. One time unit overlap is one cost unit in C.
+% First version 2015-11-02
 
-
+% Initially 0 cost
 C = 0;
 overlap= 0;
 
-n=model.n_tasks;
+% Loop over all tasks
+[no_tasks, m] = size(data.tasks);
 
-for i=1:n-1
-    start1 = model.start_t(i);
-    end1 = start1 + model.length(i);
-    for j=i+1:n
-        start2 = model.start_t(j);
-        end2 = start2 + model.length(j);
+
+for i=1:no_tasks-1
+
+    timeline1 = data.tasks(i,4);
+    % Column 6: current solution
+    start_task1 = data.tasks(i,6);
+    %min_start_task1 = data.tasks(i,2);
+    length_task1 = data.tasks(i,5);
+    end_task1 = start_task1 + length_task1;
+    
+    for j=i+1:no_tasks
+        timeline2 = data.tasks(j,4);
         
-        if start1 < start2 && start2 < end1
-            overlap = end1 - start2;
-            if end2 < end1
-                overlap = overlap - (end1 -end2);
+        % Compare tasks on the same timeline
+        if timeline1 == timeline2
+            start_task2 = data.tasks(j,6);
+            %min_start_task2 = model.tasks(j,2);
+            length_task2 =  data.tasks(j,5);
+            end_task2 = start_task2 + length_task2;
+
+            % If task two starts after task one - two cases of overlap
+            if start_task2 >= start_task1 && start_task2 <= end_task1
+                if end_task2 >= end_task1
+                    overlap = end_task1 - start_task2;
+                elseif end_task2 <= end_task1
+                    overlap = length_task2;
+                end
+
+            % If task two starts before task one - two cases of overlap
+            elseif start_task2 <= start_task1 
+                if end_task2 >= start_task1 && end_task2 <= end_task1
+                    overlap = end_task2 - start_task1;
+                elseif end_task2 >= end_task1
+                    overlap = length_task1;
+                end
             end
+            C = C + overlap
         end
-        C = C + overlap;
     end   
 end
 
-
-% 
-% Old code
-% n=numel(tour);
-% 
-% tour=[tour tour(1)];
-% 
-% C=0;
-% 
-% for k=1:n
-%     
-%     i=tour(k);
-%     j=tour(k+1);
-%     
-%     L=L+model.d(i,j);
-%     
-% end
-
-% Comment: can tasks be placed at the exact same time?
 
 end
